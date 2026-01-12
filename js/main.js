268d8e7be08872c9d029f452c5fe91d8c8f58371
teamrockun <110669802+teamrockun@users.noreply.github.com>
2026-01-12 22:02:52 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 'use strict';
 
+// modal-Window
 {
   const open = document.getElementById('open');
   const close = document.getElementById('close');
+  const modalClose = document.getElementById('modal-close');
   const modal = document.getElementById('modal');
   const mask = document.getElementById('mask');
 
@@ -12,6 +14,11 @@
   });
 
   close.addEventListener('click', () => {
+    modal.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+
+  modalClose.addEventListener('click', () => {
     modal.classList.add('hidden');
     mask.classList.add('hidden');
   });
